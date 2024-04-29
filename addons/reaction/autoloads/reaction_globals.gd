682285f5f2c6f8c9407657cf25f0df950f5fb182
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2024-04-28 19:31:49 -0700

    Added new cresource for database data and a global variables for reaction database

--- addons/reaction/autoloads/reaction_globals.gd
+++ addons/reaction/autoloads/reaction_globals.gd
@@ -1,3 +1,7 @@
 extends Node
 
 @onready var global_context: ReactionBlackboard = ReactionBlackboard.new()
+
+var databases: Dictionary = {}
+
+var current_database_uid: String
---
+++ addons/reaction/scripts/ReactionDatabase.gd
@@ -0,0 +1,6 @@
+class_name ReactionDatabase
+extends Resource
+
+@export var database_name: String
+@export var uid: String = Uuid.v4()
+@export var events: Dictionary = {}
