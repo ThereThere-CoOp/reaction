b465753a848f062656ec2148020a87934c422f03
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2024-05-01 13:22:08 -0700

    Updated change database path buttons to use folder icon

--- addons/reaction/views/settings_view.gd
+++ addons/reaction/views/settings_view.gd
@@ -5,16 +5,26 @@ signal databases_path_changed()
 
 const ReactionSettings = preload("../utilities/settings.gd")
 
-@onready var databases_path_dialog: FileDialog = %DatabasesFolderDialog
+@onready var change_database_path_button: Button = %ChangeDatabasesPathButton
 @onready var databases_path_lineedit: LineEdit = %DatabasesPathLineEdit
+# dialogs
+@onready var databases_path_dialog: FileDialog = %DatabasesFolderDialog
 
 
 func _ready() -> void:
+	call_deferred("apply_theme")
+	
 	# initializing settings control
 	databases_path_lineedit.text = ReactionSettings.get_setting(
 		ReactionSettings.DATABASES_PATH_SETTING_NAME,
 		ReactionSettings.DATABASES_PATH_SETTING_DEFAULT
 	)
+
+
+func apply_theme() -> void:
+	# Simple check if onready
+	if is_instance_valid(change_database_path_button):
+		change_database_path_button.icon = get_theme_icon("Folder", "EditorIcons")
 
 
 func _on_change_databases_path_button_pressed():
