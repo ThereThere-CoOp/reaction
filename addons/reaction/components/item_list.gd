a0169561b2b28b1d48c19d83f22053237d2d8b84
Eduardo Miguel Hernández <32711490+edwardc91@users.noreply.github.com>
2024-05-17 18:37:19 -0700

    Added documentation comments on ReactionDatabase object and changed vent list item to use custom scene to list item

--- addons/reaction/components/item_list.gd
+++ addons/reaction/components/item_list.gd
@@ -12,8 +12,7 @@ var undo_redo: EditorUndoRedoManager:
 		undo_redo = next_undo_redo
 	get:
 		return undo_redo
-		
-		
+
 var current_database: ReactionDatabase
 
 var current_item: Resource
@@ -38,19 +37,21 @@ var current_item_index: int = -1
 @onready var items_list = %ItemList
 @onready var add_item_button = %AddItemButton
 @onready var remove_item_button = %RemoveItemButton
-
+@onready var item_searcher_edit: LineEdit = %ItemSearcher
 
 var _processed_item_text: String
 
+
 func _ready():
-	item_label.text = "%s:" % _processed_item_text
+	item_label.text = "%ss:" % _processed_item_text
 	add_item_button.text = "Add %s" % _processed_item_text
 	remove_item_button.text = "Remove %s" % _processed_item_text
-	
+	item_searcher_edit.placeholder_text = "Search by %s's name" % _processed_item_text
+
 	if not items_list.is_anything_selected():
 		remove_item_button.disabled = true
-		
-		
+
+
 func setup_items(database: ReactionDatabase) -> void:
 	items_list.clear()
 	remove_item_button.disabled = true
@@ -59,33 +60,33 @@ func setup_items(database: ReactionDatabase) -> void:
 	for item in current_database.get(item_database_field_name).values():
 		var index = items_list.add_item(item.get(item_name_field))
 		items_list.set_item_metadata(index, item)
-		
-	
+
+
 func _select_item(index: int, is_emit_signal: bool = true) -> void:
 	current_item = items_list.get_item_metadata(index)
 	current_item_index = index
 	items_list.select(index)
-	
+
 	if is_emit_signal:
 		item_selected.emit(current_item)
-	
+
 	remove_item_button.disabled = false
-	
-	
+
+
 func _add_item(item: Resource, index_to_add: int = -1) -> void:
 	var add_function_callable = Callable(current_database, add_function_name)
 	add_function_callable.call(item)
 	var index = items_list.add_item(item.get(item_name_field))
 	items_list.set_item_metadata(index, item)
-	
+
 	if index_to_add != -1:
 		items_list.move_item(index, index_to_add)
 		index = index_to_add
-	
+
 	_select_item(index, false)
 	item_added.emit(index, item)
-	
-	
+
+
 func _remove_item(item: Resource, index: int) -> void:
 	var add_function_callable = Callable(current_database, remove_function_name)
 	add_function_callable.call(item.uid)
@@ -103,9 +104,9 @@ func _remove_item(item: Resource, index: int) -> void:
 			current_item_index -= 1
 			current_item_index = max(0, current_item_index)
 			current_item = items_list.get_item_metadata(current_item_index)
-			
+
 		_select_item(current_item_index, false)
-			
+
 	item_removed.emit(index, item)
 
 
@@ -124,18 +125,12 @@ func _on_add_item_button_pressed():
 	var new_item = reaction_resource.get_new_object()
 	undo_redo.create_action("Add %s" % _processed_item_text)
 	undo_redo.add_do_method(self, "_add_item", new_item)
-	undo_redo.add_undo_method(
-		self, "_remove_item", new_item, current_database.global_facts.size()
-	)
+	undo_redo.add_undo_method(self, "_remove_item", new_item, current_database.global_facts.size())
 	undo_redo.commit_action()
 
 
 func _on_remove_item_button_pressed():
 	undo_redo.create_action("Remove  %s" % _processed_item_text)
-	undo_redo.add_do_method(
-		self,
-		"_remove_item",
-			current_item, current_item_index
-	)
+	undo_redo.add_do_method(self, "_remove_item", current_item, current_item_index)
 	undo_redo.add_undo_method(self, "_add_item", current_item, current_item_index)
 	undo_redo.commit_action()
--- addons/reaction/scripts/ReactionDatabase.gd
+++ addons/reaction/scripts/ReactionDatabase.gd
@@ -1,6 +1,12 @@
 @tool
 class_name ReactionDatabase
 extends Resource
+## ----------------------------------------------------------------------------[br]
+## Resource item to storage a reaction database.
+##
+## A database contains all the events and global facts and responses.
+## available. [br]
+## ----------------------------------------------------------------------------
 
 const ReactionSettings = preload("../utilities/settings.gd")
 
@@ -13,44 +19,104 @@ const ReactionSettings = preload("../utilities/settings.gd")
 @export var global_facts: Dictionary = {}
 
 
-func create_new_fact():
+## ----------------------------------------------------------------------------[br]
+## Create a clean fact and add it to the database 
+## numbers [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
+func create_new_fact() -> void:
 	var fact = ReactionFactItem.new()
 	global_facts[fact.uid] = fact
 	save_data()
 
 
-func add_fact(fact: ReactionFactItem):
+## ----------------------------------------------------------------------------[br]
+## Add a given fact to the database
+## [br]
+## [b]Parameter(s):[/b] [br]
+## [b]* fact | ReactionFactItem:[/b] New fact to add [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
+func add_fact(fact: ReactionFactItem) -> void:
 	global_facts[fact.uid] = fact
 	save_data()
 
 
-func remove_fact(fact_uid: String):
+## ----------------------------------------------------------------------------[br]
+## Remove a given fact from the database
+## [br]
+## [b]Parameter(s):[/b] [br]
+## [b]* fact_uid | String:[/b] The fact's uid [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
+func remove_fact(fact_uid: String) -> void:
 	global_facts.erase(fact_uid)
 	save_data()
-	
 
-func add_event(event: ReactionEventItem):
+
+## ----------------------------------------------------------------------------[br]
+## Add a given event to the database
+## [br]
+## [b]Parameter(s):[/b] [br]
+## [b]* event | ReactionEventItem:[/b] New event to add [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
+func add_event(event: ReactionEventItem) -> void:
 	events[event.uid] = event
 	save_data()
-	
-	
+
+
+## ----------------------------------------------------------------------------[br]
+## Remove a given event from the database
+## [br]
+## [b]Parameter(s):[/b] [br]
+## [b]* event_uid | String:[/b] The event's uid [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
 func remove_event(event_uid: String) -> void:
 	events.erase(event_uid)
 	save_data()
-	
-	
-func add_rule_to_event(event_uid: String, rule_uid: String) -> void:
-	var event: ReactionEventItem = events[event_uid]
-	event.remove_rule(rule_uid)
-	save_data()
-	
-	
-func remove_rule_from_event(event_uid: String, new_rule: ReactionRuleItem) -> void:
-	var event: ReactionEventItem = events[event_uid]
-	event.add_rule(new_rule)
-	save_data()
 
 
+## ----------------------------------------------------------------------------[br]
+## Add a given rule to an event of the database. If event do not exists 
+## on the database will be not added
+## [br]
+## [b]Parameter(s):[/b] [br]
+## [b]* event_uid | String:[/b] Event/s uid where add the rule [br]
+## [b]* new_rule | ReactionRuleItem:[/b] New rule to be added [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
+func add_rule_to_event(event_uid: String, new_rule: ReactionRuleItem) -> void:
+	if events.has(event_uid):
+		var event: ReactionEventItem = events[event_uid]
+		event.add_rule(new_rule)
+		save_data()
+
+
+## ----------------------------------------------------------------------------[br]
+## Remove a given rule from an event of the database. If the event do not exists 
+## on the database will be not removed.
+## [br]
+## [b]Parameter(s):[/b] [br]
+## [b]* event_uid | String:[/b] Event/s uid to remove the rule [br]
+## [b]* rule_uid | String:[/b] Uid of the rule to remove [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
+func remove_rule_from_event(event_uid: String, rule_uid: String) -> void:
+	if events.has(event_uid):
+		var event: ReactionEventItem = events[event_uid]
+		event.remove_rule(rule_uid)
+		save_data()
+
+
+## ----------------------------------------------------------------------------[br]
+## Save the database to a file .tres on the path defined on the setting
+## defined with name on DATABASES_PATH_SETTING_NAME. The file will be named
+## with a combination of the database's name and the database's uid.
+## [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
 func save_data() -> void:
 	ResourceSaver.save(
 		self,
@@ -68,18 +134,23 @@ func save_data() -> void:
 	)
 
 
+## ----------------------------------------------------------------------------[br]
+## Remove the database's file .tres saved on the path defined on the setting
+## defined with name on DATABASES_PATH_SETTING_NAME if exists. 
+## [br]
+## [b]Returns: void [br]
+## ----------------------------------------------------------------------------
 func remove_savedata() -> void:
 	var databases_path = ReactionSettings.get_setting(
 		ReactionSettings.DATABASES_PATH_SETTING_NAME,
 		ReactionSettings.DATABASES_PATH_SETTING_DEFAULT
 	)
 
-	var file_path = (
-		"%s/%s_%s.tres" % [databases_path, label.replace(" ", "_"), uid]
-	)
+	var file_path = "%s/%s_%s.tres" % [databases_path, label.replace(" ", "_"), uid]
 
 	if FileAccess.file_exists(file_path):
 		DirAccess.remove_absolute(file_path)
+
 
 func _to_string():
 	return "%s_%s" % [label, uid]
