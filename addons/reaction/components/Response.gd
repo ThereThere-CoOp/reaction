3dd4f3bfbb388d9a57b3e64a303b7d86fb1af426
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2024-06-10 22:12:09 -0700

    Added extra logic to response group  and response ui components

--- addons/reaction/components/Response.gd
+++ addons/reaction/components/Response.gd
@@ -2,15 +2,36 @@
 class_name ResponseComponent
 extends HBoxContainer
 
+var index: int = -1
+
+var response_object : ReactionResponseBaseItem
+
+var current_parent_object: Resource
+
+@onready var index_label : Label = %IndexLabel
+@onready var response_container : MarginContainer = %ResponseContainer
+@onready var response_button : Button = %ResponseButton
 @onready var remove_response_button: Button = %RemoveResponseButon
 
 
-func setup(response: ReactionResponseBaseItem) -> void:
-	pass
+# call it before add to the scene
+func setup(response: ReactionResponseBaseItem, parent_object: Resource, new_index: int) -> void:
+	response_object = response
+	current_parent_object = parent_object
+	index = new_index
 	
 	
 func _ready():
 	call_deferred("apply_theme")
+	
+	index_label.text = "#%s:" % (index + 1)
+	
+	if response_object is ReactionResponseGroupItem:
+		response_button.visible = false
+	else:
+		response_button.visible = true
+		response_button.text = response_object.label
+		
 	
 	
 func apply_theme() -> void:
--- addons/reaction/components/ResponseGroup.gd
+++ addons/reaction/components/ResponseGroup.gd
@@ -2,8 +2,11 @@
 class_name ResponseGroup
 extends MarginContainer
 
+@export var responses_types: PackedStringArray = ["Dialog Response"]
 
+@onready var add_response_menu : MenuButton = %AddResponsesMenuButton
 @onready var show_hide_responses_button : CheckButton = %ShowHideResponsesButton
+@onready var responses_container : VBoxContainer = %ResponsesContainer
 @onready var responses_scroll_container : ScrollContainer = %ResponsesScrollContainer
 @onready var _responses_scrollbar: VScrollBar = responses_scroll_container.get_v_scroll_bar()
 
@@ -11,6 +14,11 @@ var _responses_scroll_to_end = false
 
 func _ready():
 	_responses_scrollbar.changed.connect(_on_responses_scroll_changed)
+	
+	var responses_menu_popup = add_response_menu.get_popup()
+	responses_menu_popup.clear()
+	for response_type in responses_types:
+		responses_menu_popup.add_item(response_type)
 
 
 func _on_responses_scroll_changed() -> void:
