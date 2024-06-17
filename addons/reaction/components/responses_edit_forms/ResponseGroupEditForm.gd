af8276802c256a6cb320d5e5bd4ebb127e4445c6
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2024-06-17 10:07:16 -0700

    Added iput to change dialog text on dialog responses edit window

--- addons/reaction/components/responses_edit_forms/ResponseGroupEditForm.gd
+++ addons/reaction/components/responses_edit_forms/ResponseGroupEditForm.gd
@@ -2,3 +2,39 @@
 class_name ResponseGroupEditForm
 extends MainResponseEditForm
 
+
+@onready var dialog_text_edit: TextEdit = %DialogTextEdit
+@onready var save_text_button: Button = %SaveTextButton
+
+
+func _ready():
+	super()
+	
+	call_deferred("apply_theme")
+	
+	if current_response.dialog_text:
+		dialog_text_edit.text = current_response.dialog_text
+	
+	
+func apply_theme() -> void:
+	# Simple check if onready
+	if is_instance_valid(save_text_button):
+		save_text_button.icon = get_theme_icon("Save", "EditorIcons")
+	
+
+### signals
+
+
+func _on_main_view_theme_changed() -> void:
+	apply_theme()
+
+
+func _on_save_text_button_pressed():
+	current_response.dialog_text = dialog_text_edit.text
+	current_database.save_data()
+	save_text_button.disabled = true
+
+
+func _on_dialog_text_edit_text_changed():
+	if dialog_text_edit.text and dialog_text_edit.text != "":
+		save_text_button.disabled = false
