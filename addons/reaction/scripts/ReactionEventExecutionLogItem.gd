62c81bc19978cc2424b5d7562e736d56590e0ba4
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2024-07-29 18:07:16 -0700

    Added documentation to event execution log resource

--- addons/reaction/scripts/ReactionEventExecutionLogItem.gd
+++ addons/reaction/scripts/ReactionEventExecutionLogItem.gd
@@ -1,19 +1,31 @@
 @tool
 class_name ReactionEventExecutionLogItem
 extends RefCounted
+## ----------------------------------------------------------------------------[br]
+## Resource use to store events execution log
+##
+## Store the event called, the rule that matches, the blackboard before and
+## after the modifications were called and if choice executed
+## [br]
+## ----------------------------------------------------------------------------
 
 
 var label: String = "LOG_LABEL"
 
 var uid: String = Uuid.v4()
 
+## choice triggered if the case
 var choice_triggered: ReactionDialogChoiceItem
 
+## stored event triggered
 var event_triggered: ReactionEventItem
+## stored rule triggered
 var rule_triggered: ReactionRuleItem
 
 var trigger_by_event: ReactionEventItem
 var trigger_by_rule: ReactionRuleItem
 
+## blackboard status before modifications applied
 var old_blackboard: ReactionBlackboard
+## blackboard status before modifications applied
 var new_blackboard: ReactionBlackboard
