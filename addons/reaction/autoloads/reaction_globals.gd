7428429b1d91b5677c44439379bb30394e998f55
Eduardo Miguel Hernández <emiguelc1991@protonmail.com>
2024-08-31 15:08:27 -0700

    Added inventory context to save data functions

--- addons/reaction/autoloads/reaction_globals.gd
+++ addons/reaction/autoloads/reaction_globals.gd
@@ -56,6 +56,9 @@ func _ready():
 func save_all_contexts_data():
 	global_context.save_data()
 	## add below extra custom added context to save their data too
+	consumables_inventory_context.save_data()
+	equipments_inventory_context.save_data()
+	goods_inventory_context.save_data()
 		
 		
 func get_default_database() -> ReactionDatabase:
