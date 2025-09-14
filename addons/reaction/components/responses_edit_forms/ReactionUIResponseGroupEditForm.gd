1f208b67ed854c3c369e0211d27f6ab077d52277
Eduardo Miguel Hernandez <emiguelc1991@protonmail.com>
2025-09-14 18:05:29 -0500

    Added initial component and modification for responses group responses settings forms

--- addons/reaction/components/responses_edit_forms/ReactionUIResponseGroupEditForm.gd
+++ addons/reaction/components/responses_edit_forms/ReactionUIResponseGroupEditForm.gd
@@ -1,3 +1,24 @@
 @tool
 class_name ReactionUIResponseGroupEditForm
 extends ReactionUIMainResponseEditForm
+
+
+@onready var responses_config_form_scene: PackedScene = preload("res://addons/reaction/components/responses_edit_forms/responses_groups_components/ReactionUIResponseGroupResponseConfig.tscn")
+
+var responses_config_container: VBoxContainer
+
+func setup(response: ReactionResponseBaseItem) -> void:
+	super(response)
+	
+	responses_config_container = %ResponsesConfigContainer
+	
+	if response:
+		responses_config_form_scene = preload("res://addons/reaction/components/responses_edit_forms/responses_groups_components/ReactionUIResponseGroupResponseConfig.tscn") 
+		var config_where = "parent_group_id = %d" % response.sqlite_id
+		var responses_data = sqlite_database.select_rows("response_parent_group_rel", config_where, ["*"])
+		
+		for data in responses_data:
+			var response_config: ReactionUIResponseGroupResponseConfig = responses_config_form_scene.instantiate()
+			response_config.setup(response, data)
+			responses_config_container.add_child(response_config)
+	
--- addons/reaction/components/responses_edit_forms/responses_groups_components/ReactionUIResponseGroupResponseConfig.gd
+++ addons/reaction/components/responses_edit_forms/responses_groups_components/ReactionUIResponseGroupResponseConfig.gd
@@ -1,4 +1,5 @@
 @tool
+class_name ReactionUIResponseGroupResponseConfig
 extends HBoxContainer
 
 @onready var response_name_lineedit: LineEdit = %ResponseNameLineEdit
@@ -6,14 +7,31 @@ extends HBoxContainer
 
 @onready var execution_order_container: VBoxContainer = %ExecutionOrderContainer
 @onready var weigth_container: VBoxContainer = %WeightContainer
+@onready var function_button: Button = %FunctionButton
+@onready var function_confirmation_dialog: ConfirmationDialog = %FactsFunctionConfirmationDialog
 
 @onready var order_spinbox: SpinBox = %OrderSpinBox
 @onready var return_once_check: CheckButton = %ReturnOnceCheckButton
 
+var order_line_edit: LineEdit
+
 var _current_data: Dictionary
 
 func setup(response_group: ReactionResponseGroupItem, data: Dictionary):
+	response_name_lineedit = %ResponseNameLineEdit
+	response_uid_lineedit = %ResponseUidLineEdit
+
+	execution_order_container = %ExecutionOrderContainer
+	weigth_container = %WeightContainer
+	function_button = %FunctionButton
+	function_confirmation_dialog = %FactsFunctionConfirmationDialog
+
+	order_spinbox = %OrderSpinBox
+	return_once_check = %ReturnOnceCheckButton
+
 	_current_data = data
+	
+	order_line_edit = order_spinbox.get_line_edit()
 	
 	execution_order_container.visible = false
 	weigth_container.visible = false
